6507d6ad1db952d29ecd204a1ac867424a90eb11
vortex <vortex@debian.VortexFG>
2023-08-18 14:28:39 +0100

    debug...

--- simplesql/__init__.py
+++ simplesql/__init__.py
@@ -147,7 +147,7 @@ class Table(object):
             else:
                 sqlcomd = "SELECT * FROM "+str(self.tb_name)+" WHERE "+str(code)+" = '"+str(value)+"'"
             stats,info = self.database.exec(sqlcomd)
-            print(stats,info)
+            print(stats,info.fetchall())
             if stats:
                 for i in info.fetchall():
                     yield i
